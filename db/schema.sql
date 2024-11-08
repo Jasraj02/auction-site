dbbf1bc3fbf6da3e4e6c2722d0ce8f921bd077db
vox-codes <69287735+vox-codes@users.noreply.github.com>
2024-11-08 19:06:00 +0000

    Updated schema given 8/11 meeting

--- db/schema.sql
+++ db/schema.sql
@@ -1,25 +1,44 @@
+DROP DATABASE IF EXISTS auctionSite;
+
+CREATE DATABASE auctionSite
+DEFAULT CHARACTER SET utf8
+DEFAULT COLLATE utf8_general_ci;
+
+USE auctionSite;
+
 CREATE TABLE Users (
     userID INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
+    firstName VARCHAR(60) NOT NULL,
+    lastName VARCHAR(60) NOT NULL,
     address VARCHAR(250) NOT NULL,
     telephoneNumber VARCHAR(20) NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
     password VARCHAR(60) NOT NULL
-);
+)
+ENGINE = InnoDB;
 
 CREATE TABLE Buyers (
     buyerID INT PRIMARY KEY,
     FOREIGN KEY (buyerID) REFERENCES Users(userID)
-);
+)
+ENGINE = InnoDB;
 
 CREATE TABLE Sellers (
     sellerID INT PRIMARY KEY,
     FOREIGN KEY (sellerID) REFERENCES Users(userID)
-);
+)
+ENGINE = InnoDB;
 
+CREATE TABLE Categories (
+	categoryID INT AUTO_INCREMENT PRIMARY KEY,
+    categoryType VARCHAR(60) NOT NULL
+)
+ENGINE = InnoDB;
 
 CREATE TABLE Auctions (
 	auctionID INT AUTO_INCREMENT PRIMARY KEY,
+    sellerID INT NOT NULL,
+    categoryID INT NOT NULL,
     FOREIGN KEY (sellerID) REFERENCES Sellers(sellerID),
     FOREIGN KEY (categoryID) REFERENCES Categories(categoryID),
     description VARCHAR(250) NOT NULL, 
@@ -29,63 +48,94 @@ CREATE TABLE Auctions (
     currentPrice DECIMAL(10, 2) NOT NULL,
     startTime TIMESTAMP NOT NULL, 
     endTime TIMESTAMP NOT NULL
-);   
-
-CREATE TABLE Categories (
-	categoryID INT AUTO_INCREMENT PRIMARY KEY,
-    categoryType VARCHAR(60) NOT NULL
-);
+)
+ENGINE = InnoDB;
 
 CREATE TABLE UserViews (
+	userID INT NOT NULL,
+    auctionID INT NOT NULL,
     FOREIGN KEY (userID) REFERENCES Users(userID),
     FOREIGN KEY (auctionID) REFERENCES Auctions(auctionID),
     viewTime TIMESTAMP NOT NULL
-);
+)
+ENGINE = InnoDB;
 
 CREATE TABLE Bids (
 	bidID INT AUTO_INCREMENT PRIMARY KEY,
+    buyerID INT NOT NULL,
+    auctionID INT NOT NULL,
     FOREIGN KEY (buyerID) REFERENCES Buyers(buyerID),
- 	FOREIGN KEY (auctionID) REFERENCES Auctions(auctionID)   
-);
+ 	FOREIGN KEY (auctionID) REFERENCES Auctions(auctionID),
+    bidPrice DECIMAL(10, 2) NOT NULL  
+)
+ENGINE = InnoDB;
 
 CREATE TABLE Updates (
 	updateID INT AUTO_INCREMENT PRIMARY KEY 	    
-);
+)
+ENGINE = InnoDB;
 
 CREATE TABLE BuyerUpdates (
     buyerUpdateID INT PRIMARY KEY,
     FOREIGN KEY (buyerUpdateID) REFERENCES Updates(updateID),
+    buyerID INT NOT NULL,
+    auctionID INT NOT NULL,
+    bidID INT NOT NULL,
     FOREIGN KEY (buyerID) REFERENCES Buyers(buyerID),
    	FOREIGN KEY (auctionID) REFERENCES Auctions(auctionID),
-    FOREIGN KEY (bidID) REFERENCES Bids(bidID),
-    updateType VARCHAR(60),
-    updateTime TIMESTAMP NOT NULL,
-    readStatus BOOLEAN NOT NULL
-);
+    FOREIGN KEY (bidID) REFERENCES Bids(bidID)
+)
+ENGINE = InnoDB;
 
 CREATE TABLE SellerUpdates (
     sellerUpdateID INT PRIMARY KEY,
     FOREIGN KEY (sellerUpdateID) REFERENCES Updates(updateID),
+    sellerID INT NOT NULL,
+    auctionID INT NOT NULL,
+    bidID INT NOT NULL,
     FOREIGN KEY (sellerID) REFERENCES Sellers(sellerID),
    	FOREIGN KEY (auctionID) REFERENCES Auctions(auctionID),
-    FOREIGN KEY (bidID) REFERENCES Bids(bidID),
+    FOREIGN KEY (bidID) REFERENCES Bids(bidID)
+)
+ENGINE = InnoDB;
+
+CREATE TABLE UpdateProperties (
+    updateID INT NOT NULL,
+    FOREIGN KEY (updateID) REFERENCES Updates(updateID),
     updateType VARCHAR(60),
     updateTime TIMESTAMP NOT NULL,
     readStatus BOOLEAN NOT NULL
-);
+)
+ENGINE = InnoDB;
 
 CREATE TABLE Questions (
 	questionID INT AUTO_INCREMENT PRIMARY KEY,
+    auctionID INT NOT NULL,
+    buyerID INT NOT NULL,
     FOREIGN KEY (auctionID) REFERENCES Auctions(auctionID),
     FOREIGN KEY (buyerID) REFERENCES Buyers(buyerID),
     questionText VARCHAR(250) NOT NULL, 
     questionTimestamp TIMESTAMP NOT NULL, 
     responseText VARCHAR(250), 
     responseTimestamp TIMESTAMP
-);
+)
+ENGINE = InnoDB;
 
 CREATE TABLE Watchlists (
+    buyerID INT NOT NULL,
+    auctionID INT NOT NULL,
 	FOREIGN KEY (buyerID) REFERENCES Buyers(buyerID),
     FOREIGN KEY (auctionID) REFERENCES Auctions(auctionID),
     notificationEnabled BOOLEAN NOT NULL
-);    
+)
+ENGINE = InnoDB;
+
+CREATE TABLE Recommendations (
+    recommendationID INT AUTO_INCREMENT PRIMARY KEY,
+    buyerID INT NOT NULL,
+    auctionID INT NOT NULL,
+    FOREIGN KEY (buyerID) REFERENCES Buyers(buyerID),
+    FOREIGN KEY (auctionID) REFERENCES Auctions(auctionID),
+    recommendationScore DECIMAL(3, 2) NOT NULL
+)
+ENGINE = InnoDB;
