07a52538c9ab35b0ddf8117d0419181bfd14b3ef
hannzhao4 <hannzhao@outlook.com>
2024-11-19 21:15:52 +0000

    Add files via upload
    Implemented search function and users can visually re-arrange listings of items within categories.
    Users can also order by popularity based on bids, price and auction end date. Need to add option for users to order by popularity based on user views.
    
    Updated version with changed table names to be consistent with SQL database.

--- browse.php
+++ browse.php
@@ -1,5 +1,6 @@
-<?php include_once("header.php")?>
-<?php require("utilities.php")?>
+<?php include_once("header.php"); ?>
+<?php require("utilities.php"); ?>
+<?php require_once("database.php"); ?>
 
 <div class="container">
 
@@ -14,34 +15,61 @@
     <div class="col-md-5 pr-0">
       <div class="form-group">
         <label for="keyword" class="sr-only">Search keyword:</label>
-	    <div class="input-group">
+        <div class="input-group">
           <div class="input-group-prepend">
             <span class="input-group-text bg-transparent pr-0 text-muted">
               <i class="fa fa-search"></i>
             </span>
           </div>
-          <input type="text" class="form-control border-left-0" id="keyword" placeholder="Search for anything">
+          <input type="text" class="form-control border-left-0" id="keyword" name="keyword" placeholder="Search for anything">
         </div>
       </div>
     </div>
     <div class="col-md-3 pr-0">
       <div class="form-group">
         <label for="cat" class="sr-only">Search within:</label>
-        <select class="form-control" id="cat">
-          <option selected value="all">All categories</option>
-          <option value="fill">Fill me in</option>
-          <option value="with">with options</option>
-          <option value="populated">populated from a database?</option>
+        <select class="form-control" id="cat" name="cat">
+          <option value="all">All categories</option>
+          <?php
+          //create dropdown menu for categories
+            $searchQuery = "SELECT categoryID, categoryType FROM Categories ORDER BY categoryType";
+            $searchResult = mysqli_query($connection, $searchQuery);
+            
+            while ($category = mysqli_fetch_assoc($searchResult)) {
+              $selected = '';
+              if (isset($_GET['cat']) && $_GET['cat'] == $category['categoryID']) {
+                  $selected = 'selected';
+              }
+              echo "<option value='" . $category['categoryID'] . "' $selected>" . 
+                   htmlspecialchars($category['categoryType']) . "</option>";
+            }
+          ?>
         </select>
       </div>
     </div>
     <div class="col-md-3 pr-0">
       <div class="form-inline">
         <label class="mx-2" for="order_by">Sort by:</label>
-        <select class="form-control" id="order_by">
-          <option selected value="pricelow">Price (low to high)</option>
-          <option value="pricehigh">Price (high to low)</option>
-          <option value="date">Soonest expiry</option>
+        <select class="form-control" id="order_by" name="order_by">
+<?php
+// allow users to choose sorting option and makes sure sorting option is maintained when page is refreshed
+?>
+          <option value="popularityByBids" <?php echo (isset($_GET['order_by']) && $_GET['order_by'] == 'popularityByBids') ? 'selected' : ''; ?>>
+            Popularity: by bids
+          </option> 
+          <option value="priceHighToLow" <?php echo (isset($_GET['order_by']) && $_GET['order_by'] == 'priceHighToLow') ? 'selected' : ''; ?>>
+            Price: highest first
+          </option>         
+          <option value="priceLowToHigh" <?php echo (isset($_GET['order_by']) && $_GET['order_by'] == 'priceLowToHigh') ? 'selected' : ''; ?>>
+            Price: lowest first
+          </option>
+          <option value="date" <?php echo (isset($_GET['order_by']) && $_GET['order_by'] == 'date') ? 'selected' : ''; ?>>
+            Time: ending soonest
+          </option>
+<?php
+  //****need to add option to sort popularity by user views*****
+  ?>
+          </option>    
         </select>
       </div>
     </div>
@@ -50,149 +78,179 @@
     </div>
   </div>
 </form>
-</div> <!-- end search specs bar -->
+</div>  <!-- end search specs bar -->
 
 
 </div>
-
 <?php
-  // Retrieve these from the URL
-  if (!isset($_GET['keyword'])) {
-    // TODO: Define behavior if a keyword has not been specified.
-  }
-  else {
-    $keyword = $_GET['keyword'];
-  }
 
-  if (!isset($_GET['cat'])) {
-    // TODO: Define behavior if a category has not been specified.
-  }
-  else {
-    $category = $_GET['cat'];
-  }
-  
-  if (!isset($_GET['order_by'])) {
-    // TODO: Define behavior if an order_by value has not been specified.
-  }
-  else {
-    $ordering = $_GET['order_by'];
-  }
-  
-  if (!isset($_GET['page'])) {
-    $curr_page = 1;
-  }
-  else {
-    $curr_page = $_GET['page'];
-  }
+//default for keyword is empty
+if (!isset($_GET['keyword'])) {
+  $keyword = '';
+} else {
+  $keyword = $_GET['keyword'];
+}
 
-  /* TODO: Use above values to construct a query. Use this query to 
-     retrieve data from the database. (If there is no form data entered,
-     decide on appropriate default value/default query to make. */
-  
-  /* For the purposes of pagination, it would also be helpful to know the
-     total number of results that satisfy the above query */
-  $num_results = 96; // TODO: Calculate me for real
-  $results_per_page = 10;
-  $max_page = ceil($num_results / $results_per_page);
+//default for categories is all
+if (!isset($_GET['cat'])) {
+  $category = 'all';
+} else {
+  $category = $_GET['cat'];
+}
+
+//if order_by value not specified, sort by popularity of bids
+if (!isset($_GET['order_by'])) {
+  $ordering = 'popularityByBids';
+} else {
+  $ordering = $_GET['order_by'];
+}
+
+if (!isset($_GET['page'])) {
+  $curr_page = 1;
+} else {
+  $curr_page = $_GET['page'];
+}
+
+//query to retrieve data from the database
+$searchQuery = "SELECT Auctions.*,  MAX(Bids.bidPrice) as currentPrice, COUNT(Bids.bidID) as numberBids
+                FROM Auctions
+                LEFT JOIN Bids ON Auctions.auctionID = Bids.auctionID
+                WHERE Auctions.endTime>CURRENT_TIMESTAMP()";
+
+if (!empty($keyword)) {
+  $searchQuery .= " AND (Auctions.auctionDescription LIKE '%$keyword%')";
+}
+if ($category != 'all') {
+  $searchQuery .= " AND Auctions.categoryID = '$category'";
+}
+$searchQuery .= " GROUP BY Auctions.auctionID";
+
+//order results to match sorting option, if multiple items have same value order alphabetically
+switch ($ordering) {
+  case  'priceLowtoHigh':
+  case 'priceLowToHigh':
+    $searchQuery .= " ORDER BY COALESCE(MAX(Bids.bidPrice), Auctions.startingPrice) ASC, Auctions.auctionDescription ASC";
+    break;
+  case 'priceHighToLow':
+    $searchQuery .= " ORDER BY COALESCE(MAX(Bids.bidPrice), Auctions.startingPrice) DESC, Auctions.auctionDescription ASC";
+    break;
+  case 'date':
+    $searchQuery .= " ORDER BY Auctions.endTime ASC, Auctions.auctionDescription ASC";
+    break;
+  case 'popularityByBids':
+    $searchQuery .= " ORDER BY numberBids DESC, Auctions.auctionDescription ASC";
+    break;
+}
+
+//Pagination
+//limit number of items per page
+$resultsPerPage = 10;
+$offset = ($curr_page - 1) * $resultsPerPage;
+$searchQuery .= " LIMIT $resultsPerPage OFFSET $offset";
+
+//calculate number of items that match criteria
+$paginationQuery = "SELECT COUNT(DISTINCT Auctions.auctionID) AS numberRows
+                    FROM Auctions
+                    LEFT JOIN Bids ON Auctions.auctionID = Bids.auctionID
+                    WHERE Auctions.endTime>CURRENT_TIMESTAMP()";
+
+if (!empty($keyword)) {
+  $paginationQuery .= " AND (Auctions.auctionDescription LIKE '%$keyword%')";
+}
+if ($category != 'all') {
+  $paginationQuery .= " AND Auctions.categoryID = '$category'";
+}
+
+//calculate number of pages
+$resultsQuery = mysqli_query($connection, $paginationQuery);
+$numResults = mysqli_fetch_assoc($resultsQuery);
+$max_page = ceil($numResults['numberRows'] / $resultsPerPage);
+
+$searchResult = mysqli_query($connection, $searchQuery);
+
 ?>
 
 <div class="container mt-5">
-
-<!-- TODO: If result set is empty, print an informative message. Otherwise... -->
-
 <ul class="list-group">
 
-<!-- TODO: Use a while loop to print a list item for each auction listing
-     retrieved from the query -->
-
 <?php
-  // Demonstration of what listings will look like using dummy data.
-  $item_id = "87021";
-  $title = "Dummy title";
-  $description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eget rutrum ipsum. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Phasellus feugiat, ipsum vel egestas elementum, sem mi vestibulum eros, et facilisis dui nisi eget metus. In non elit felis. Ut lacus sem, pulvinar ultricies pretium sed, viverra ac sapien. Vivamus condimentum aliquam rutrum. Phasellus iaculis faucibus pellentesque. Sed sem urna, maximus vitae cursus id, malesuada nec lectus. Vestibulum scelerisque vulputate elit ut laoreet. Praesent vitae orci sed metus varius posuere sagittis non mi.";
-  $current_price = 30;
-  $num_bids = 1;
-  $end_date = new DateTime('2020-09-16T11:00:00');
-  
-  // This uses a function defined in utilities.php
-  print_listing_li($item_id, $title, $description, $current_price, $num_bids, $end_date);
-  
-  $item_id = "516";
-  $title = "Different title";
-  $description = "Very short description.";
-  $current_price = 13.50;
-  $num_bids = 3;
-  $end_date = new DateTime('2020-11-02T00:00:00');
-  
-  print_listing_li($item_id, $title, $description, $current_price, $num_bids, $end_date);
-?>
 
+//error messages
+if (!$searchResult) {
+  echo "<div class='alert alert-danger'>Error executing query: " . mysqli_error($connection) ;
+} elseif (mysqli_num_rows($searchResult) == 0) {
+  echo '<div class="alert alert-info">No auction results match your search criteria.';
+} 
+
+//while loop to print a list item for each auction listing retrieved from the query
+else {
+  while ($row = mysqli_fetch_assoc($searchResult)) {
+    $currentPrice = $row['currentPrice'] ?? $row['startingPrice'];
+    $endDate = new DateTime($row['endTime']);
+
+    //function in utilities.php
+    print_listing_li($row['auctionID'], $row['auctionDescription'], substr($row['auctionDescription'], 0, 200) . '...',
+      $currentPrice, $row['numberBids'], $endDate); }
+}
+?>
 </ul>
 
-<!-- Pagination for results listings -->
 <nav aria-label="Search results pages" class="mt-5">
   <ul class="pagination justify-content-center">
-  
-<?php
+  <?php
 
-  // Copy any currently-set GET variables to the URL.
-  $querystring = "";
-  foreach ($_GET as $key => $value) {
-    if ($key != "page") {
-      $querystring .= "$key=$value&amp;";
+    // Copy any currently-set GET variables to the URL.
+    $querystring = "";
+    foreach ($_GET as $key => $value) {
+      if ($key != "page") {
+        $querystring .= "$key=$value&amp;";
+      }
     }
-  }
-  
-  $high_page_boost = max(3 - $curr_page, 0);
-  $low_page_boost = max(2 - ($max_page - $curr_page), 0);
-  $low_page = max(1, $curr_page - 2 - $low_page_boost);
-  $high_page = min($max_page, $curr_page + 2 + $high_page_boost);
-  
-  if ($curr_page != 1) {
-    echo('
-    <li class="page-item">
-      <a class="page-link" href="browse.php?' . $querystring . 'page=' . ($curr_page - 1) . '" aria-label="Previous">
-        <span aria-hidden="true"><i class="fa fa-arrow-left"></i></span>
-        <span class="sr-only">Previous</span>
-      </a>
-    </li>');
-  }
-    
-  for ($i = $low_page; $i <= $high_page; $i++) {
-    if ($i == $curr_page) {
-      // Highlight the link
+
+    $high_page_boost = max(3 - $curr_page, 0);
+    $low_page_boost = max(2 - ($max_page - $curr_page), 0);
+    $low_page = max(1, $curr_page - 2 - $low_page_boost);
+    $high_page = min($max_page, $curr_page + 2 + $high_page_boost);
+
+    if ($curr_page != 1) {
+      echo('<li class="page-item"><a class="page-link" href="browse.php?' . $querystring . 'page=' . ($curr_page - 1) . '"><i class="fa fa-arrow-left"></i></a></li>');
+    }
+    for ($i = $low_page; $i <= $high_page; $i++) {
+      if ($i == $curr_page) {
+        // Highlight the link 
+        echo('
+        <li class="page-item active">');
+      } 
+      else {
+           // Non-highlighted link
+        echo('
+      <li class="page-item">');
+      }
+
+      // Do this in any case
       echo('
-    <li class="page-item active">');
+        <a class="page-link" href="browse.php?' . $querystring . 'page=' . $i . '">' . $i . '</a>
+      </li>');
     }
-    else {
-      // Non-highlighted link
-      echo('
-    <li class="page-item">');
-    }
-    
-    // Do this in any case
-    echo('
-      <a class="page-link" href="browse.php?' . $querystring . 'page=' . $i . '">' . $i . '</a>
-    </li>');
-  }
   
-  if ($curr_page != $max_page) {
-    echo('
-    <li class="page-item">
-      <a class="page-link" href="browse.php?' . $querystring . 'page=' . ($curr_page + 1) . '" aria-label="Next">
-        <span aria-hidden="true"><i class="fa fa-arrow-right"></i></span>
-        <span class="sr-only">Next</span>
-      </a>
-    </li>');
-  }
-?>
 
-  </ul>
-</nav>
-
-
-</div>
-
-
-
-<?php include_once("footer.php")?>
+    if ($curr_page != $max_page) {
+      echo('
+      <li class="page-item">
+        <a class="page-link" href="browse.php?' . $querystring . 'page=' . ($curr_page + 1) . '" aria-label="Next">
+          <span aria-hidden="true"><i class="fa fa-arrow-right"></i></span>
+          <span class="sr-only">Next</span>
+        </a>
+      </li>');
+    }
+  ?>
+  
+    </ul>
+  </nav>
+  
+  
+  </div>
+  
+  
+  
+  <?php include_once("footer.php")?>
