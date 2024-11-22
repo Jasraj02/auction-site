bd6cd60287f05a2d99bc95a360e3586750a3145a
Jasraj Aulakh <177463743+Jasraj02@users.noreply.github.com>
2024-11-22 23:48:01 +0000

    Fix bids code so only logged in users make bids
    Previously, a non-logged in user could make bids
    and whilst these wouldn't be saved in the Bids
    table,they would be saved within the Auction
    table. This interfered with auction pricing.

--- place_bid.php
+++ place_bid.php
@@ -1,5 +1,9 @@
-<?php include 'database.php'; ?>
+<?php include_once 'header.php'; ?>
+<?php include_once 'database.php'; ?>
 <?php
+
+
+
 // TODO: Extract $_POST variables, check they're OK, and attempt to make a bid.
 // Notify user of success/failure and redirect/give navigation options.
 $bid_price = $_POST["bid"];
@@ -13,9 +17,23 @@ while ($row = $result->fetch_assoc()) {
     // [Yan TODO]: Exception Handling
     $current_price = $row["currentPrice"];
 }
+
+//check if user is logged in
+$loggedIn = false;
+if (isset($_SESSION['logged_in']) && $_SESSION['logged_in'] === true) {
+    $loggedIn = true;
+}
+
+// prevent a user not logged in from placing a bid
+if (!$loggedIn) {
+    echo '<div class="alert alert-danger">Must be logged in to place a bid.</div>';
+    header("refresh: 2; url=$previous_url");
+    exit();
+}
+
 if ($bid_price > $current_price) {
     echo "Success.";
-    // https://www.w3schools.com/mysql/mysql_update.asp
+    // https://www.w3schoolsgit .com/mysql/mysql_update.asp
     $query = "UPDATE Auctions SET currentPrice = $bid_price WHERE auctionID = $item_id";
     mysqli_query($connection, $query) or die("Error making query to database.");
     $query = "INSERT INTO Bids (buyerID, auctionID, bidPrice) VALUES ($user_id, $item_id, $bid_price)";
@@ -28,5 +46,5 @@ echo "<br>Redirecting to previous page.";
 
 // Project Tutorial 3 PHP and MySQL, slide 31
 // https://stackoverflow.com/questions/6119451/page-redirect-after-certain-time-php
-header("refresh: 5; url=$previous_url");
+header("refresh: 2; url=$previous_url");
 ?>
